214f8886b4d51b23a14cab5c5378beef20ed2081
nabilaaurelia28 <nabila.aurelia18208@student.unsika.ac.id>
2022-03-11 16:09:15 +0700

    add to

---
+++ app/src/main/java/com/example/chalenge2/LayoutGojek.kt
@@ -0,0 +1,11 @@
+package com.example.chalenge2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LayoutGojek : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout_gojek)
+    }
+}
